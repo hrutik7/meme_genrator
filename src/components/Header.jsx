7b7076e70b27909cfb32be4d3e9759bdf69790b1
hrutik7 <hrutik.kumthekar19@vit.edu>
2022-03-17 18:41:04 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./head.css"
+const Header = () => {
+  return (
+    <header>
+      <img
+        src='http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png'
+        alt='Problem?'
+      />
+      <p>Meme Generator</p>
+
+      <img
+        src='http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png'
+        alt='Problem?'
+        className='img_2'
+      />
+    </header>
+  )
+}
+
+export default Header
